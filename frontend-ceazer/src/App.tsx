95bfb609b7330d7357126edb5e97fb6cfc647919
Gary2412 <ghua5155@uni.sydney.edu.au>
2025-01-14 20:57:39 +1100

    added login and register tsx and routing

--- frontend-ceazer/src/App.tsx
+++ frontend-ceazer/src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import * as Account from './pages/';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Account.Login />} />
+        <Route path="/Register" element={<Account.Register />} />
       </Routes>
     </Router>
   );
---
+++ frontend-ceazer/src/pages/account/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login: React.FC = () => {
+
+    return <>
+
+
+
+    </>
+
+
+}
+
+export default Login
