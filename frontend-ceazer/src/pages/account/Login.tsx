31c91e1dfd3fbd61f2665d32be14abbed0f0045a
Gary <ghua5155@wsl>
2025-01-15 02:39:05 +1100

    added email, password, submit in login page

--- frontend-ceazer/src/pages/account/Login.tsx
+++ frontend-ceazer/src/pages/account/Login.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-
+import React, { useState, } from 'react'
+import styles from './Login.module.css'
 const Login: React.FC = () => {
 
     return <>
+        <h2>Log In</h2>
+        <div>
+            <form className={styles.formContainer}>
+                <input className={styles.email} placeholder="Email" type="email" />
+                <input className={styles.password} placeholder="Password" type="password" />
+                <button className={styles.button} type="submit"> Log In </button>
 
-
+            </form>
+        </div>
 
     </>
 
