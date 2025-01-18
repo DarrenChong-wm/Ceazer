175cc00ab1aa6e599b84bd1654d1047cfb560e01
Gary <ghua5155@wsl>
2025-01-19 01:14:12 +1100

    added floating labels in login and button hover

--- frontend-ceazer/src/pages/account/Login.tsx
+++ frontend-ceazer/src/pages/account/Login.tsx
@@ -3,12 +3,18 @@ import styles from './Login.module.css'
 const Login: React.FC = () => {
 
     return <>
-        <h2>Log In</h2>
-        <div>
+        {/* <h2>Log In</h2> */}
+        <div className={styles.wrapper}>
             <form className={styles.formContainer}>
-                <input className={styles.email} placeholder="Email" type="email" />
-                <input className={styles.password} placeholder="Password" type="password" />
-                <button className={styles.button} type="submit"> Log In </button>
+                <div className={styles.inputContainer}>
+                    <input className={styles.email} placeholder="" type="email" />
+                    <label className={styles.floatingLabel}>Email</label>
+                </div>
+                <div className={styles.inputContainer}>
+                    <input className={styles.password} placeholder="" type="password" />
+                    <label className={styles.floatingLabel}>Password</label>
+                </div>
+                <button className={styles.button} type="submit"> LOG IN </button>
 
             </form>
         </div>
